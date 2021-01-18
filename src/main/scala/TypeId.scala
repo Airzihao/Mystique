4d9f60d69ffdafa73fc775566ad73d9905a66fa6
Airzihao <airzihao@163.com>
2021-01-18 22:03:20 +0800

    TypeId

---
+++ src/main/scala/TypeId.scala
@@ -0,0 +1,17 @@
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created in 18:37 2021/1/18
+ * @Modified By:
+ */
+object TypeId {
+  val INT = 1
+  val SHORT = 2
+  val LONG = 3
+  val FLOAT = 4
+  val DOUBLE = 5
+  val CHAR = 6
+  val STRING = 7
+  val BOOLEAN = 8
+
+}
